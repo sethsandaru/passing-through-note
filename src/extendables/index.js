9d329114547c5a7f3785904255fb8c855f2e138c
Phat Tran <prosandaru@gmail.com>
2020-05-15 23:46:03 +0700

    Installed SocketIO and VueSocket

--- src/extendables/index.js
+++ src/extendables/index.js
@@ -15,7 +15,11 @@ import {AjaxPlugin} from "@/extendables/ajax-plugin";
 import {TopBarPlugin} from "@/extendables/topbar-plugin";
 import Toaster from 'v-toaster'
 import {LoadingPlugin} from "@/extendables/loading-plugin";
+import socketio from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io';
+import {REST_CONFIG} from "@/configs/rest";
 
+export const SocketInstance = socketio(REST_CONFIG.BASE_URL);
 
 /**
  * Register here
@@ -26,7 +30,7 @@ Vue.use(AjaxPlugin); // Added by Phat Tran
 Vue.use(TopBarPlugin); // Added by Phat Tran
 Vue.use(Toaster); // Added by Phat Tran
 Vue.use(LoadingPlugin); // Added by Phat Tran
-
+Vue.use(VueSocketIO, SocketInstance)  // Added by Phat Tran
 
 
 // Please make sure to keep a newline after injected a new plugin
